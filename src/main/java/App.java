b1c733ab04d3cdd87a2230f18aa38402eb49abe8
danielle-santos <danielle.danyy92santos@gmail.com>
2019-03-01 22:40:47 -0300

    Maven

---
+++ src/main/java/App.java
@@ -0,0 +1,9 @@
+
+public class App {
+	public static void main(String[] args) {
+		String[] resultado = ResultadoMegasena.obtemUltimoResultado();
+		for (String string : resultado) {
+			System.out.println(string + " ");
+		}
+	}
+}
